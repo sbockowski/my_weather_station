e2272882d6e9f3b26320b81535249786138f4034
Szymon Boćkowski <sbockowskix@gmail.com>
2026-08-04 17:31:46 +0200

    Create base endpoints

--- web_app/app/main.py
+++ web_app/app/main.py
@@ -1,6 +1,9 @@
+from sqlmodel import select, Session
+
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
-from app.database import create_db_and_tables
+from fastapi import Depends, FastAPI
+from web_app.app.database import create_db_and_tables, get_session
+from web_app.app.models import Measurement
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -8,3 +11,26 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+def read_root():
+    return {"message": "Stacja pogodowa działa!"}
+
+
+@app.post("/api/v1/measurements", response_model=Measurement)
+def add_measurement(
+    measurement: Measurement,
+    session: Session = Depends(get_session),
+):
+    session.add(measurement)
+    session.commit()
+    session.refresh(measurement)
+    return measurement
+
+
+@app.get("/api/v1/measurements", response_model=list[Measurement])
+async def get_measurements(
+    session: Session = Depends(get_session),
+):
+    measurements = session.exec(select(Measurement)).all()
+    return measurements
