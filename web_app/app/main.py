0ec6e92e8303b08e25527c784dbc5b55214b49f0
Szymon Boćkowski <sbockowskix@gmail.com>
2026-08-04 16:52:53 +0200

    Create FastAPI app

---
+++ web_app/app/main.py
@@ -0,0 +1,10 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from app.database import create_db_and_tables
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
